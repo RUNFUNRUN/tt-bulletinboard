c27a6e2c66ede702a3b69832b404ccea6a5c5346
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2025-02-22 18:12:33 +0900

    install tailwindcss and clear contents

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './app.tsx';
 import './index.css';
-import App from './App.tsx';
 
 // biome-ignore lint: ini vite app
 createRoot(document.getElementById('root')!).render(
